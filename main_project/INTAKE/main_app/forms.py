ef9df20f8061966ded4c047f5cc88d7f2b26f2d5
HSH4U2U <hsh700788@naver.com>
2018-08-14 14:55:03 +0900

    오늘은 화요일~

--- main_project/INTAKE/main_app/forms.py
+++ main_project/INTAKE/main_app/forms.py
@@ -5,4 +5,5 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = '__all__'      # 여기 content와 star만 들어가게 나머지는 알아서 값 넣을 수 있도록 구현
+        fields = '__all__'
+        # fields = ['star', 'content', ]
--- main_project/INTAKE/main_app/views.py
+++ main_project/INTAKE/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CommentForm
-from .models import Comment, Product
+from .models import Product
+from django.contrib.auth.decorators import permission_required
 
 
 # Create your views here.
@@ -12,14 +13,23 @@ def product(request, pk):
     # 상품별 페이지 생성
     product = get_object_or_404(Product, pk=pk)
 
-    # 후기 시스템 구현
+    # 후기 등록 시스템 구현(상품 보여주는 페이지와 같은 곳에서 보이게 구현함)
     form = CommentForm(request.POST, request.FILES)
     if request.method == 'POST':
-        if form.is_valid():
-            comment = form.save()
-            return redirect(comment)
-    else:
-        form = CommentForm()
+        # 로그인 되었을 때만 쓸 수 있게 할 생각
+        # test = permission_required(perm, login_url=None, raise_exception=False)
+        # if test is True:        # 위에 perm만 이해하면 될 듯
+            if form.is_valid():
+                comment = form.save()
+
+                # comment.author = request.user
+                # comment.product = product
+
+                return redirect(comment)
+            else:
+                form = CommentForm()
+        # else:
+        #     form = CommentForm()
 
     # 여기 아래 아마도 상품 정보랑 html 연결 구현
     ctx = {
